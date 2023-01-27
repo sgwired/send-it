e08dc117aa6ea967ee3f71e85ad6d7033456f4ce
jakkstoyss <jakkstoyss@gmail.com>
2023-01-26 16:27:53 -0800

    inital basic site

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+renderTemplate(w, "about.page.tmpl")
+}
+
+
+func main() {
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	http.ListenAndServe(portNumber, nil)
+
+}
+
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template", err)
+	}
+}
